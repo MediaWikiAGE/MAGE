95346ab0ebaa7b95f6145583f87c034f175974c4
Helcostr <helcostr@gmail.com>
2020-11-25 22:20:02 -0800

    User authentication (addSingleUser) (#3)
    * .gitignore (package-lock.json)
    
    * .gitignore pnpm lock
    
    * Update package.json
    
    keytar
    tailwindcss
    
    * Create vue.config.js
    
    Add vue config to work with keytar
    
    * Add Dummy Keytar
    
    Also shift package lock ignores
    
    * User Storage
    
    - Add default file with default settings (to be tweaked)
    - Add error detecction to replace config
    - Flesh out some getters and setters for storage
    
    * AddSingleUser
    
    - Sorry for bad eslint
    - Use Key Based Users
    
    * Fix eslint
    
    * Add key-spacing afterColon lint
    
    * Space while destructuring
    
    * @Helcostr Add object-curly-spacing always lint
    
    * move ESLint to json config
    
    * Expand eslint json to match others
    
    * Alphabetize ESlint JSON

--- src/background.js
+++ src/background.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 "use strict";
 
 import path from "path";
@@ -7,9 +6,14 @@ import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 import defaultSettings from "./spellbook.json";
-
-//Practice Keytar (and data storage)
+import { get } from "got";
+import http from "http";
+import https from "https";
+import Bot from "@sidemen19/mediawiki.js";
 import keytar from "keytar";
+
+//Should hook into node project varaible
+const projectName = "MediaWikiAGE";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
@@ -17,18 +21,51 @@ const isMac = process.platform !== "darwin";
 
 //Load Config
 const spellbook = {
+  settingFileError: false,
   settings: defaultSettings,
   set set(add) {
     this.settings = add;
   },
   set addUserData(add) {
-    this.settings.users.push(add);
+    this.settings.users[add.key] = { ...(this.settings.users[add.key]||{}), ...add.val };
   },
   set addSiteData(add) {
-    this.settings.sites[add.key] = add.val;
+    this.settings.sites[add.key] = { ...(this.settings.sites[add.key]||{}), ...add.val };
   },
   set addFarmData(add) {
-    this.settings.farms[add.key] = add.val;
+    this.settings.farms[add.key] = { ...(this.settings.farms[add.key]||{}), ...add.val };
+  },
+  loadSettings: function() {
+    //Load Settings
+    try {
+      this.set = JSON.parse(fs.readFileSync(path.join(app.getPath("userData"), "spellbook.json")));
+    } catch (err) {
+      if (err.name === "SyntaxError") {
+        this.settingFileError = true;
+        console.error("Spellbook bad json. Warn user here, and do NOT overwrite their filesave. Suggest to load over from scratch");
+      } else {
+        //DEFAULT SETTINGS SAVE IF NO FILE DETECTED (ASSUME FIRST STARTUP)
+        this.saveSettings();
+        //Delete keytar as well
+        keytar.findCredentials(projectName).forEach(obj => keytar.deletePassword(projectName, obj.account));
+      }
+    }
+  },
+  saveSettings: function() {
+    //PROMPT USER TO OVERWRITE BAD FILE SETTING???
+    const overwrite = true;
+    if (!this.settingFileError || overwrite) {
+      fs.writeFileSync(path.join(app.getPath("userData"), "spellbook.json"), this.export);
+
+      //Flush keytar on a True Overwrite
+      if(this.settingFileError) {
+        this.settingFileError = false;
+        keytar.findCredentials(projectName).forEach(obj => keytar.deletePassword(projectName, obj.account));
+      }
+    }
+  },
+  get toString() {
+    return JSON.stringify(this.settings);
   },
   get get() {
     return this.settings;
@@ -45,17 +82,104 @@ const spellbook = {
   get getFarm() {
     return this.settings.farms;
   },
-  addSingleUser: function(username, password, url, note) { }
-};
+  addSingleUser: function(username, password, url, note) {
+    /*Probable structure
+    let userOut = {
+        "site": "genshin-impact-1",
+        "note": "Main Account",
+        "isBot": true,
+        "theme": "light"
+    }*/
+    class ErrInput extends Error {
+      constructor(msg) {
+        super(msg);
+        this.name = "ErrInput";
+      }
+    }
+    if (typeof username === "undefined" || username.length === 0)
+      throw new ErrInput("Username can't be undefined");
+    if (typeof password === "undefined" || password.length === 0)
+      throw new ErrInput("Password can't be undefined");
+    const scriptPath = new URL(url);
 
-try {
-  spellbook.set = JSON.parse(fs.readFileSync(path.join(app.getPath("userData"), "spellbook.json")));
-} catch (err) {
-  if (err.name === "SyntaxError")
-    console.error("Spellbook bad json");
-  else
-    fs.writeFileSync(path.join(app.getPath("userData"), "spellbook.json"), spellbook.export);
-}
+    //Chunk Load Script Path
+    new Promise((resolve, reject) => {
+      (scriptPath.protocol === "https:"?https:http).request({
+          method: "GET",
+          port: scriptPath.protocol === "https:"?443:80,
+          hostname: scriptPath.hostname,
+          path: scriptPath.pathname,
+        },
+        function(response, err) {
+          const request = this;
+          let body = "";
+
+          //Abort when match chunk
+          response.on("data", chunk => {
+            body+=chunk;
+            const match = body.match(/(?:src|href)="(.+)(?:load|api)\.php/);            
+            if (match)
+              request.abort();
+          });
+
+          response.on("end", () => {
+            //Parse Script Path
+            const match = body.match(/(?:src|href)="(.+)(?:load|api)\.php/);
+            if (!match)
+              throw new ErrInput("URL is (probably) not a MediaWiki url.");
+            resolve(match[1]);
+          });
+        }).on("error", reject).end();
+    }).then(path => {
+      scriptPath.pathname=path;
+      //Get clean server and scriptpath values from api.php
+      return get(`${scriptPath.href}api.php?action=query&meta=siteinfo&type=login&format=json`, { responseType: "json" });
+    }).then(async resp => {
+      const siteinfo = resp.body.query;
+
+      //Site data
+      const temp = {};
+      ["articlepath", "scriptpath", "lang", "server", "generator"].forEach(key => temp[key] = siteinfo.general[key]);
+      const siteKey = `${siteinfo.general.server+siteinfo.general.scriptpath}|${siteinfo.general.wikiid}`;
+
+      const bot = new Bot({
+        server: siteinfo.general.server,
+        path: `${siteinfo.general.scriptpath}/`,
+        botUsername: username,
+        botPassword: password
+      });
+      const loginResult = await bot.login();
+      if (loginResult.login.result === "Failed")
+          console.log(loginResult.login.reason);
+      else {
+        const whoResult = await bot.whoAmI();
+        const userOut = {
+          username: username,
+          site: siteKey
+        };
+        ["name", "groups", "rights"].forEach(key => userOut[key]= whoResult[key]);
+        this.addUserData = {
+          key: `${siteKey}|${username}`,
+          val: userOut
+        };
+        this.addSiteData={
+          key: siteKey,
+          val: temp
+        };
+        this.saveSettings();
+        keytar.setPassword(projectName, `${siteKey}|${username}`, password);
+      }
+    });
+  },
+  addFarm: function(username, password, defaultNote) {
+
+  },
+  addFarmUser: function(farm, url, note) {
+
+  }
+};
+spellbook.loadSettings();
+spellbook.addSingleUser("Echoblast53@Testing", "dummypasswordforgithub", "https://genshin-impact.fandom.com/es/api.php");
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
