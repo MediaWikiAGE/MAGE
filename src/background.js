4b979a1937f8b35bba5ad323e205711e7af2eeb1
Helcostr <15018549+Helcostr@users.noreply.github.com>
2020-11-25 20:55:07 -0800

    AddSingleUser
    - Sorry for bad eslint
    - Use Key Based Users

--- src/background.js
+++ src/background.js
@@ -7,9 +7,13 @@ import { app, protocol, BrowserWindow } from 'electron';
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import defaultSettings from './spellbook.json';
-
-//Practice Keytar (and data storage)
+import {get} from 'got';
+import http from 'http';
+import https from 'https';
+import Bot from '@sidemen19/mediawiki.js';
 import keytar from 'keytar';
+
+const projectName = "MediaWikiAGE"; //Should hook into node project varaible
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -17,18 +21,48 @@ const isMac = process.platform !== 'darwin';
 
 //Load Config
 const spellbook = {
+  settingFileError: false,
   settings: defaultSettings,
   set set(add) {
     this.settings = add;
   },
   set addUserData(add) {
-    this.settings.users.push(add);
+    this.settings.users[add.key] = {...(this.settings.users[add.key]||{}),...add.val};
   },
-  set addSiteData(add){
-    this.settings.sites[add.key] = add.val;
+  set addSiteData(add) {
+    this.settings.sites[add.key] = {...(this.settings.sites[add.key]||{}), ...add.val};
   },
-  set addFarmData(add){
-    this.settings.farms[add.key] = add.val;
+  set addFarmData(add) {
+    this.settings.farms[add.key] = {...(this.settings.farms[add.key]||{}), ...add.val};
+  },
+  loadSettings:function() {
+    //Load Settings
+    try {
+      this.set = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'),'spellbook.json')));
+    } catch (err){
+      if (err.name === "SyntaxError") {
+        this.settingFileError = true;
+        console.error("Spellbook bad json. Warn user here, and do NOT overwrite their filesave. Suggest to load over from scratch");
+      } else {
+        //DEFAULT SETTINGS SAVE IF NO FILE DETECTED (ASSUME FIRST STARTUP)
+        this.saveSettings();
+        //Delete keytar as well
+        keytar.findCredentials(projectName).forEach(obj=>keytar.deletePassword(projectName, obj.account));
+      }
+    }
+  },
+  saveSettings:function(){
+    let overwrite = true;//PROMPT USER TO OVERWRITE BAD FILE SETTING???
+    if (!this.settingFileError || overwrite) {
+      fs.writeFileSync(path.join(app.getPath('userData'),'spellbook.json'),this.export);
+      if(this.settingFileError) {//Flush keytar on a True Overwrite
+        this.settingFileError = false;
+        keytar.findCredentials(projectName).forEach(obj=>keytar.deletePassword(projectName, obj.account));
+      }
+    }
+  },
+  get toString() {
+    return JSON.stringify(this.settings);
   },
   get get() {
     return this.settings;
@@ -46,16 +80,103 @@ const spellbook = {
     return this.settings.farms;
   },
   addSingleUser: function(username,password,url,note) {
-};
+    /*Probable structure
+    let userOut = {
+        "site": "genshin-impact-1",
+        "note": "Main Account",
+        "isBot": true,
+        "theme": "light"
+    }*/
+    class ErrInput extends Error {
+      constructor(msg) {
+        super(msg);
+        this.name = 'ErrInput';
+      }
+    }
+    if (typeof username === "undefined" || username.length === 0)
+      throw new ErrInput("Username can't be undefined");
+    if (typeof password === "undefined" || password.length === 0)
+      throw new ErrInput("Password can't be undefined");
+    let scriptPath = new URL(url);
 
-try {
-  spellbook.set = JSON.parse(fs.readFileSync(path.join(app.getPath('userData'),'spellbook.json')));
-} catch (err){
-  if (err.name === "SyntaxError")
-    console.error("Spellbook bad json");
-  else
-    fs.writeFileSync(path.join(app.getPath('userData'),'spellbook.json'),spellbook.export);
-}
+    //Chunk Load Script Path
+    new Promise((resolve,reject)=>{
+      (scriptPath.protocol === "https:"?https:http).request({
+          method: "GET",
+          port: scriptPath.protocol === "https:"?443:80,
+          hostname: scriptPath.hostname,
+          path: scriptPath.pathname,
+        },
+        function(response, err){
+          const request = this;
+          let body = "";
+
+          //Abort when match chunk
+          response.on("data", chunk=>{
+            body+=chunk;
+            let match = body.match(/(?:src|href)="(.+)(?:load|api)\.php/);            
+            if (match)
+              request.abort();
+          });
+
+          response.on("end", ()=>{
+            //Parse Script Path
+            let match = body.match(/(?:src|href)="(.+)(?:load|api)\.php/);
+            if (!match)
+              throw new ErrInput("URL is (probably) not a MediaWiki url.");
+            resolve(match[1]);
+          });
+        }).on('error',reject).end();
+    }).then(path=>{
+      scriptPath.pathname=path
+      //Get clean server and scriptpath values from api.php
+      return get(scriptPath.href+"api.php?action=query&meta=siteinfo&type=login&format=json",{responseType:'json'})
+    }).then(async resp=>{
+      let siteinfo = resp.body.query;
+
+      //Site data
+      let temp = {};
+      ['articlepath','scriptpath','lang','server','generator'].forEach(key=>temp[key] = siteinfo.general[key]);
+      let siteKey = siteinfo.general.server+siteinfo.general.scriptpath+"|"+siteinfo.general.wikiid;
+
+      const bot = new Bot({
+        server: siteinfo.general.server,
+        path: siteinfo.general.scriptpath+'/',
+        botUsername: username,
+        botPassword: password
+      });
+      let loginResult = await bot.login();
+      if (loginResult.login.result === 'Failed')
+          console.log(loginResult.login.reason);
+      else {
+        let whoResult = await bot.whoAmI();
+        let userOut = {
+          username:username,
+          site:siteKey
+        };
+        ['name','groups','rights'].forEach(key=>userOut[key]= whoResult[key]);
+        this.addUserData = {
+          key:siteKey+"|"+username,
+          val:userOut
+        };
+        this.addSiteData={
+          key:siteKey,
+          val:temp
+        };
+        this.saveSettings();
+        keytar.setPassword(projectName, siteKey+"|"+username, password);
+      }
+    });
+  },
+  addFarm:function(username,password,defaultNote){
+
+  },
+  addFarmUser: function(farm,url,note){
+
+  }
+};
+spellbook.loadSettings();
+spellbook.addSingleUser("Echoblast53@Testing","dummypasswordforgithub","https://genshin-impact.fandom.com/es/api.php");
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -77,7 +198,7 @@ async function createWindow() {
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    //if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
     createProtocol('app');
     // Load the index.html when not in development
