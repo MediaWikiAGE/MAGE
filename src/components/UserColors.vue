7397dd21d233b4898303a04baebdc7bab72ff74e
DSquirrelGM <42658845+DSquirrelGM@users.noreply.github.com>
2020-11-27 08:06:01 -0500

    Update UserColors.vue (#8)

--- src/components/UserColors.vue
+++ src/components/UserColors.vue
@@ -1,110 +1,52 @@
 <template>
-<div
-  id="UserColors"
-  v-bind:style="{ color: colorMain, 'background-color': backgroundMain }"
- >
-  <input
-    type="checkbox"
-    id="editUserColors"
-    v-model="editUserColors"
-    v-on:input="$emit('input', $event.target.value)"
-   >
-  <label
-    for="editUserColors"
-    v-bind:style="{ color: colorMain, 'background-color': backgroundMain }"
-   >
-    Edit User Colors
-   </label>
-  <div 
-    id="panelUserColors"
-   >
-    <div
-      v-bind:style="{ color: colorMain, background: backgroundMain }"
-     >
-      <input
-        type="text"
-        v-model="colorMain"
-        v-bind:style="{ border: '3px inset inherit', color: colorMain, background: backgroundMain }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="colorMain" >
-        Main Window Color
-       </label>
-      <br>
-      <input
-        type="text"
-        v-model="backgroundMain"
-        v-bind:style="{ border: '3px inset inherit', color: colorMain, background: backgroundMain }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="backgroundMain" >
-        Main Window Background
-       </label>
-     </div>
-    <div
-      v-bind:style="{ border: '3px inset inherit', color: colorMenu, background: backgroundMenu }"
-     >
-      <input
-        type="text"
-        v-model="colorMenu"
-        v-bind:style="{ border: '3px inset inherit', color: colorMenu, background: backgroundMenu }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="colorMenu" >
-        Menu Color
-       </label>
-      <br>
-      <input
-        type="text"
-        v-model="backgroundMenu"
-        v-bind:style="{ border: '3px inset inherit', color: colorMenu, background: backgroundMenu }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="backgroundMenu" >
-        Menu Background
-       </label>
-     </div>
-    <div
-      v-bind:style="{ color: colorError, background: backgroundError }"
-     >
-      <input
-        type="text"
-        v-model="colorError"
-        v-bind:style="{ border: '3px inset inherit', color: colorError, background: backgroundError }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="colorError" >
-        Error Color
-       </label>
-      <br>
-      <input
-        type="text"
-        v-model="backgroundError"
-        v-bind:style="{ border: '3px inset inherit', color: colorError, 'background-color': backgroundError }"
-        v-on:input="$emit('input', $event.target.value)"
-       >
-      <label for="backgroundError" >
-        Error Background
-       </label>
-     </div>
-   </div>
- </div>
+<div id="UserColors" :style="{ display: 'inline-block', border: '6px groove ' + colorMain, color: colorMain, background: backgroundMain }" >
+    <input :style="{ border: '1px solid ' + colorMain, color: colorMain, background: backgroundMain, width: '1em' }" type="password" max-length="1" length="1" id="editUserColors" v-model="editUserColors" v-on:input="$emit('input', $event.target.value);" >
+    <label for="editUserColors" :style="{ color: colorMain, background: backgroundMain }" >
+        Edit User Colors
+      </label>
+    <div id="panelUserColors" :style="{ display: ( editUserColors ? 'inline-block' : 'none' ) }">
+        <div :style="{ border: '6px double ' + colorMain, color: colorMain, background: backgroundMain }" >
+            <input type="text" v-model="colorMain" :style="{ border: '2px inset ' + colorMain, color: colorMain, background: backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="colorMain" >
+                Main Window Color
+              </label>
+            <br>
+            <input type="text" v-model="backgroundMain" :style="{ border: '2px inset ' + colorMain, color: colorMain, background: backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="backgroundMain" >
+                Main Window Background
+              </label>
+          </div>
+        <div :style="{ border: '6px double ' + colorMain, color: colorMenu, background: backgroundMenu }" >
+            <input type="text" v-model="colorMenu" :style="{ border: '2px inset' + colorMain, color: colorMenu, background: backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="colorMenu" >
+                Menu Color
+              </label>
+            <br>
+            <input type="text" v-model="backgroundMenu" :style="{ border: '2px inset' + colorMain, color: colorMenu, background: backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="backgroundMenu" >
+                Menu Background
+              </label>
+          </div>
+        <div v-bind:style="{ border: '6px double ' + colorMain, color: colorError, background: backgroundError }" >
+            <input type="text" v-model="colorError" :style="{ border: '2px inset ' + colorMain, color: colorError, background: backgroundError }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="colorError" >
+                Error Color
+              </label>
+            <br>
+            <input type="text" v-model="backgroundError" :style="{ border: '2px inset ' + colorMain, color: colorError, 'background-color': backgroundError }" v-on:input="$emit('input', $event.target.value)" >
+            <label for="backgroundError" >
+                Error Background
+              </label>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "UserColors",
   data() {
-    return {
-      colorMain: "#ddd",
-      backgroundMain: "#222",
-      colorMenu: "#fff",
-      backgroundMenu: "#000",
-      colorError: "#700",
-      backgroundError: "#300",
-      editUserColors: true,
-      ntv: true
-    };
+    return { colorMain: "#fff", backgroundMain: "#000", colorMenu: "#777", backgroundMenu: "#333", colorError: "#700", backgroundError: "#300", editUserColors: "", ntv: true };
   },
   el: "#app",
   mounted() {
