8c502b75ba972d90943422aa90808fbdb9077379
DSquirrelGM <42658845+DSquirrelGM@users.noreply.github.com>
2020-11-27 23:41:29 -0500

    vertical collapsed layout - color menu panel
    changed layout of color menu panel to vertical, set to always collapsed until clicked, changed edit toggle button type/styles

--- src/components/UserColors.vue
+++ src/components/UserColors.vue
@@ -1,42 +1,33 @@
 <template>
-<div id="UserColors" :style="{ display: 'inline-block', border: '6px groove ' + colorMain, color: colorMain, background: backgroundMain }" >
-    <input :style="{ border: '1px solid ' + colorMain, color: colorMain, background: backgroundMain, width: '1em' }" type="password" max-length="1" length="1" id="editUserColors" v-model="editUserColors" v-on:input="$emit('input', $event.target.value);" >
-    <label for="editUserColors" :style="{ color: colorMain, background: backgroundMain }" >
-        Edit User Colors
-      </label>
+<div id="UserColors" :style="{ 'display':'inline-block', 'color':'transparent', 'background':'transparent', 'text-align':'center' }">
+    <div id="editUserColors" v-on:click="editUserColors = (!editUserColors);" :style="{ 'border': '3px outset ' + colorMenu, 'background': backgroundMenu, 'color': colorMenu }"> Edit User Colors </div>
     <div id="panelUserColors" :style="{ display: ( editUserColors ? 'inline-block' : 'none' ) }">
-        <div :style="{ border: '6px double ' + colorMain, color: colorMain, background: backgroundMain }" >
-            <input type="text" v-model="colorMain" :style="{ border: '2px inset ' + colorMain, color: colorMain, background: backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="colorMain" >
-                Main Window Color
-              </label>
+        <div :style="{ 'border':'6px double ' + colorMain, 'color':colorMain, 'background':backgroundMain }" >
+            <label for="colorMain" > Window Color </label>
             <br>
-            <input type="text" v-model="backgroundMain" :style="{ border: '2px inset ' + colorMain, color: colorMain, background: backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="backgroundMain" >
-                Main Window Background
-              </label>
+            <input type="text" v-model="colorMain" :style="{ 'border':'2px inset ' + colorMain, 'color':colorMain, 'background':backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
+            <br>
+            <label for="backgroundMain" > Window Background </label>
+            <br>
+            <input type="text" v-model="backgroundMain" :style="{ 'border':'2px inset ' + colorMain, 'color':colorMain, 'background':backgroundMain }" v-on:input="$emit('input', $event.target.value)" >
           </div>
-        <div :style="{ border: '6px double ' + colorMain, color: colorMenu, background: backgroundMenu }" >
-            <input type="text" v-model="colorMenu" :style="{ border: '2px inset' + colorMain, color: colorMenu, background: backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="colorMenu" >
-                Menu Color
-              </label>
+        <div :style="{ 'border':'6px double ' + colorMain, 'color':colorMenu, 'background':backgroundMenu }" >
+            <label for="colorMenu" > Menu Color </label>
             <br>
-            <input type="text" v-model="backgroundMenu" :style="{ border: '2px inset' + colorMain, color: colorMenu, background: backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="backgroundMenu" >
-                Menu Background
-              </label>
+            <input type="text" v-model="colorMenu" :style="{ 'border':'2px inset' + colorMain, 'color':colorMenu, 'background':backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
+            <br>
+            <label for="backgroundMenu" > Menu Background </label>
+            <br>
+            <input type="text" v-model="backgroundMenu" :style="{ 'border':'2px inset' + colorMain, 'color':colorMenu, 'background':backgroundMenu }" v-on:input="$emit('input', $event.target.value)" >
           </div>
-        <div v-bind:style="{ border: '6px double ' + colorMain, color: colorError, background: backgroundError }" >
-            <input type="text" v-model="colorError" :style="{ border: '2px inset ' + colorMain, color: colorError, background: backgroundError }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="colorError" >
-                Error Color
-              </label>
+        <div v-bind:style="{ 'border':'6px double ' + colorMain, 'color':colorError, 'background':backgroundError }" >
+            <label for="colorError" > Error Color </label>
             <br>
-            <input type="text" v-model="backgroundError" :style="{ border: '2px inset ' + colorMain, color: colorError, 'background-color': backgroundError }" v-on:input="$emit('input', $event.target.value)" >
-            <label for="backgroundError" >
-                Error Background
-              </label>
+            <input type="text" v-model="colorError" :style="{ 'border':'2px inset ' + colorMain, 'color':colorError, 'background':backgroundError }" v-on:input="$emit('input', $event.target.value)" >
+            <br>
+            <label for="backgroundError" > Error Background </label>
+            <br>
+            <input type="text" v-model="backgroundError" :style="{ 'border':'2px inset ' + colorMain, 'color':colorError, 'background-color': backgroundError }" v-on:input="$emit('input', $event.target.value)" >
           </div>
       </div>
   </div>
@@ -44,43 +35,43 @@
 
 <script>
 export default {
-  name: "UserColors",
-  data() {
-    return { colorMain: "#fff", backgroundMain: "#000", colorMenu: "#777", backgroundMenu: "#333", colorError: "#700", backgroundError: "#300", editUserColors: "", ntv: true };
-  },
-  el: "#app",
-  mounted() {
-    if (localStorage.colorMain) {
-      this.colorMain = localStorage.colorMain;
-    }
-    if (localStorage.backgroundMain) {
-      this.backgroundMain = localStorage.backgroundMain;
-    }
-    if (localStorage.colorMenu) {
-      this.colorMenu = localStorage.colorMenu;
-    }
-    if (localStorage.backgroundMenu) {
-      this.backgroundMenu = localStorage.backgroundMenu;
-    }
-    if (localStorage.colorError) {
-      this.colorError = localStorage.colorError;
-    }
-    if (localStorage.backgroundError) {
-      this.backgroundError = localStorage.backgroundError;
-    }
-    if (localStorage.editUserColors) {
-      this.editUserColors = localStorage.editUserColors;
-    }
-    this.$nextTick(function () {  });
-  },
-  watch: {
-    colorMain(newValue) { localStorage.colorMain = newValue; },
-    backgroundMain(newValue) { localStorage.backgroundMain = newValue; },
-    colorMenu(newValue) { localStorage.colorMenu = newValue; },
-    backgroundMenu(newValue) { localStorage.backgroundMenu = newValue; },
-    colorError(newValue) { localStorage.colorError = newValue; },
-    backgroundError(newValue) { localStorage.backgroundError = newValue; },
-    editUserColors(newValue) { localStorage.editUserColors = newValue; }
-  }
+
+name: "UserColors",
+
+data() {
+return { colorMain: "#fff", backgroundMain: "#000", colorMenu: "#777", backgroundMenu: "#333", colorError: "#700", backgroundError: "#300", editUserColors: false, GlobalStringCSS: "" };
+},
+
+methods: {
+
+UpdateCSS: function () {
+localStorage.colorMain = this.colorMain;
+localStorage.backgroundMain = this.backgroundMain;
+localStorage.colorMenu = this.colorMenu;
+localStorage.backgroundMenu = this.backgroundMenu;
+localStorage.colorError = this.colorError;
+localStorage.backgroundError = this.backgroundError;
+}
+
+},
+
+el: "#app",
+
+mounted() {
+if (localStorage.colorMain) this.colorMain = localStorage.colorMain;
+if (localStorage.backgroundMain) this.backgroundMain = localStorage.backgroundMain;
+if (localStorage.colorMenu) this.colorMenu = localStorage.colorMenu;
+if (localStorage.backgroundMenu) this.backgroundMenu = localStorage.backgroundMenu;
+if (localStorage.colorError) this.colorError = localStorage.colorError;
+if (localStorage.backgroundError) this.backgroundError = localStorage.backgroundError;
+this.editUserColors = false;
+this.UpdateCSS;
+}
+
 };
 </script>
+
+<style>
+body { background: backgroundMain; color: colorMain; position: relative; left: 0; top: 0; right: 0; bottom: 0; }
+#app { background: backgroundMain; color: colorMain; position: relative; left: 0; top: 0; right: 0; bottom: 0; }
+</style>
