867deb66af0fd92bd012d3cdccf94bc7f8ee21b8
Helcostr <helcostr@gmail.com>
2020-12-02 07:36:10 -0800

    Hook Current User, and Hook User List (#20)
    * Hook Current User, and Hook User List
    
    * Missing eslint

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,24 @@
     <div
       class="w-full mx-auto space-y-4 overflow-hidden text-xs sm:px-6 lg:px-8"
     >
- Logged in as John Doe @ notarealwiki.wiki
+      <template v-if="name">
+        Logged in as {{name}} @ {{wiki}}
+      </template>
+      <template v-else>
+        Not logged in
+      </template>
     </div>
   </footer>
 </template>
 
 <script>
-export default {
-  data: () => ({}),
-};
+  export default {
+    created() {
+      window.api.remote("getUser").then(data => {
+        const { name, wiki } = data||{};
+        this.name = name;
+        this.wiki = wiki;
+      });
+    }
+  };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    Hi
+    <p>Hi</p>
+    <ul>
+      <li v-for="user in users" :key="user.key" v-on:click="login" v-bind:data-id="user.key">
+        {{user.name}} {{user.groups}} {{user.server}}{{user.scriptpath}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
+  data: () => ({ users: [] }),
+  methods: {
+    login: event => {
+      event.target;
+    }
+  },
+  created() {
+    window.api.remote("getUserLists").then(data => {
+      this.users = data;
+    });
+  }
 };
 </script>
