e285d0c7598c948c0c1525c0cdfe7131f78f1cd0
DSquirrelGM <42658845+DSquirrelGM@users.noreply.github.com>
2020-12-08 23:57:35 -0500

    remove layout switch - unused

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 
 <template>
   <div id="app">
-    <AppLayout>
-      <router-view />
-    </AppLayout>
+    <div class="flex flex-col justify-between w-screen h-screen">
+      <transition name="fade" appear>
+        <main class="container p-4">
+          <MageHeader />
+          <router-view />
+          <MageFooter />
+        </main>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
+import MageHeader from "@/components/Header";
+import MageFooter from "@/components/Footer";
 export default {
   name: "App",
-};
+  components: {
+    MageHeader,
+    MageFooter,
+    }
+  };
 </script>
 
