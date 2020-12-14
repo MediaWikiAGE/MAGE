628cf8496bb7a329027624e9c7615cfe2d46af1a
Sidemen19 <lavyag01@gmail.com>
2020-12-15 08:31:10 +1100

    Add login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <b>User Lists</b> <button v-on:click="logout" class="hover:bg-purple-100 active:bg-purple-200">Logout</button> <button class="hover:bg-purple-100 active:bg-purple-200" v-on:click="disconnect">Disconnect from Server</button>
-    <ul>
-      <li v-for="user in users" :key="user.key">
-        <button v-bind:data-id="user.key" v-on:click="login" class="hover:bg-purple-100 active:bg-purple-200">{{user.name}} | {{user.server}}{{user.scriptpath}}</button> <ul class="list-disc ml-8"><li>Notes: {{user.note}}</li><li>Groups: {{user.groups}}</li><li>Farm Name: {{user.farmName}}</li><li>Farm Note: {{user.farmNote}}</li></ul>
-      </li>
-    </ul>
+    <router-link to="/login"><button @click="login">Login</button></router-link>
   </div>
 </template>
 
