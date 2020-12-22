2a2b01cf74b81ff17e21f4ab5b8c3d0179c16ce5
AttemptToCallNil <usergreenstone@gmail.com>
2020-12-23 01:03:56 +0500

    Styling changes (#50)
    * Make menu text unselectable
    
    * Set cursor to pointer when hovering over menu buttons
    
    In addition, moved some always-true classes out of ":class" and
    into "class" as these attributes can coexist.
    
    * Extract SVG icons into separate files
    
    I had to remove some `inkscape:` attributes from the MAGE logo for
    it to work.
    
    * Change "Close/Open" to "Collapse/Expand"
    
    I believe this reflects the button's meaning more accurately.
    
    * Change layout of router links in nav menu
    
    Borders were rather overloading, plus there were two buttons
    for the same exact thing.
    
    * Change text-hiding class to "hidden"
    
    "remove" actually made the text component render (somewhat), which
    caused some weird spacing issues.
    
    * Extract @click attribute
    
    * Highlight currently active tab in nav menu
    
    * Add on-hover highlighting for inactive tabs
    
    Also adjusted the colors a bit. Also getting unhappy with Tailwind
    forcing me to spell "grey" with an A.
    
    * Use chevron icons for expand/collapse
    
    I believe that makes the meaning of the button more clear.
    
    * Switch nav menu to a flex layout
    
    No longer needs some old hacks too.
    
    * Re-style the collapse/expand button to avoid borders
    
    Minor styling changes also included.
    
    * Remove redundant and/or unused classes
    
    * Ensure the class attribute is consistently placed
    
    * Remove a lot of borders
    
    Hardly critical, they cluttered the screen a lot.
    
    * Extract SVG icons into a separate component
    
    Also gets rid of the redundant icon components
    
    * Prevent dragging of icons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app" style="left: 0; top: 0; width: 100vw; height: 100vh; vertical-align: top; grid-template-columns: max-content auto; grid-gap: 0.25em;" class="main grid">
-    <div id="nav" style="left: 0; top: 0; width: auto; height: calc(100vh - 6ex); vertical-align: top; grid-template-columns: max-content auto; grid-gap: 0.25em; grid-row-start: 1; grid-column-start: 1; grid-row-end: 2;" class="menu grid">
-      <div class="relative button nopad" style="width: 3em; height: 3em;" id="buttonMobileMenu" :title="mobileMenuOpen ? 'Close Menu' : 'Open Menu'" @click="mobileMenuOpen = !mobileMenuOpen">
-        <buttonmenu style="width: 100%; height: 100%;" v-show="!mobileMenuOpen" class="nopad" /> <buttonx style="width: 100%; height: 100%;" v-show="mobileMenuOpen" class="nopad" />
+  <div id="app" class="main grid" style="left: 0; top: 0; width: 100vw; height: 100vh; vertical-align: top; grid-template-columns: max-content auto; grid-gap: 0.25em;">
+    <div id="nav" class="menu select-none" style="left: 0; top: 0; width: auto; height: calc(100vh - 6ex); vertical-align: top;">
+      <div class="flex flex-col" style="gap: 2px">
+        <div class="flex whitespace-nowrap p-0 bg-indigo-100 cursor-pointer" id="buttonMobileMenu" :title="mobileMenuOpen ? 'Collapse Menu' : 'Expand Menu'" @click="mobileMenuOpen = !mobileMenuOpen">
+          <div class="ml-auto" v-show="mobileMenuOpen"><svg-icon width="32" height="32" icon="chevron-double-left" /></div>
+          <div class="mx-auto" v-show="!mobileMenuOpen"><svg-icon width="24" height="32" icon="chevron-double-right" /></div>
+        </div>
+        <router-link custom v-for="(item, i) in this.menuItems" :key="i" :to="item.url">
+          <div class="flex items-center cursor-pointer transition duration-150 ease-in-out" :class="this.getMenuLinkClass(item.name)" @click="this.$router.push(`${item.url}`); this.activeTab = item.name;">
+            <div class="pad" :title="item.title" style="width: 48px; height: 48px;" ><svg-icon width="48" height="48" :icon="item.buttonImage" /></div>
+            <div class="pad" :class="{ hidden: !mobileMenuOpen }"> {{ item.title }} </div>
+          </div>
+        </router-link>
       </div>
-      <div :class="{ remove: !mobileMenuOpen }"> &nbsp; </div>
-      <router-link custom v-for="(item, i) in this.menuItems" :key="i" :to="item.url">
-        <div class="relative button pad" :title="item.title" style="width: 3em; height: 3em;" @click="this.$router.push(`${item.url}`)"> <component class="nopad" style="width: 100%; height: 100%;" :is="`${item.buttonimage}`" @click="this.$router.push(`${item.url}`)" /> </div>
-        <div :class="{ remove: !mobileMenuOpen, button: true, pad: true }" @click="this.$router.push(`${item.url}`)"> {{ item.title }} </div>
-      </router-link>
     </div>
     <div class="pad main" style="width: 100%; height: calc(100vh - 6ex); grid-row-start: 1; grid-column-start: 2;"> <router-view /> </div>
     <div class="pad main" style="height: 4.5ex; grid-row-start: 2; grid-column-start: 2;"> <MageFooter /> </div>
@@ -17,12 +21,8 @@
 
 <script>
   import MageFooter from "@/components/Footer";
-  import magelogo from "@/magelogo.vue";
-  import buttonmenu from "@/buttonmenu.vue";
-  import buttonx from "@/buttonx.vue";
-  import buttonidtag from "@/buttonidtag.vue";
-  import buttontasks from "@/buttontasks.vue";
-  import buttonabout from "@/buttonabout.vue";
+  import SvgIcon from "@/components/SvgIcon";
+  
   import { ref } from "vue";
   export default {
     name: "App",
@@ -30,12 +30,21 @@
       return {
         mobileMenuOpen: true,
         menuItems: [
-          { buttonimage: "magelogo", title: "Home", url: "/" },
-          { buttonimage: "buttonabout", title: "About", url: "/about" },
-          { buttonimage: "buttonidtag", title: "Login Manager", url: "/login" },
-          { buttonimage: "buttontasks", title: "Task List", url: "/tasks" },
+          { name: "home", buttonImage: "magelogo", title: "Home", url: "/" },
+          { name: "about", buttonImage: "tag", title: "About", url: "/about" },
+          { name: "login", buttonImage: "identification", title: "Login Manager", url: "/login" },
+          { name: "tasks", buttonImage: "pencil-alt", title: "Task List", url: "/tasks" },
         ],
+        activeTab: "home",
       };
+    },
+    methods: {
+      getMenuLinkClass: function(linkName) {
+        return {
+          "bg-gray-300": this.activeTab === linkName,
+          "hover:bg-gray-200": this.activeTab !== linkName,
+        };
+      }
     },
 
     onmount() {
@@ -46,13 +55,11 @@
       this.$nextTick(function () { });
     },
 
-    components: { MageFooter, magelogo, buttonmenu, buttonx, buttonidtag, buttontasks, buttonabout },
+    components: { MageFooter, SvgIcon },
   };
 </script>
 
 <style>
-  :root { border-collapse: separate; }
-
   #app button:focus, #app button:active,
   #app input:focus, #app input:active,
   #app a:focus, #app a:active,
@@ -66,23 +73,12 @@
   input.normal { width: 8em; }
   input.long { width: 12em; }
 
-  .absolute { position: absolute; }
-
-  .relative { position: relative; }
-
-  .grid { display: grid; }
-
   #nav {
-    display: grid;
     width: 100%;
     height: 100%;
     justify-content: start;
     align-content: start;
   }
-
-  .fixed { position: fixed; }
-
-  .dib { display: inline-block; }
 
   .hide { visibility: hidden; }
 
@@ -90,27 +86,22 @@
 
   .remove { width: 0; visibility: hidden; overflow: hidden; }
 
-  .nopad { padding: 0; }
-
   .pad { padding: 0.25em; }
 
   .menu {
     color: MenuText;
     background: Menu;
-    border: 0.125em outset ThreeDShadow;
     padding: 0.25em;
   }
 
   .main {
     color: WindowText;
     background: Window;
-    border: 0.125em inset ThreeDShadow;
     padding: 0.25em;
   }
 
   .button {
     color: ButtonText;
     background: ButtonFace;
-    border: 0.125em outset ThreeDShadow;
   }
 </style>
