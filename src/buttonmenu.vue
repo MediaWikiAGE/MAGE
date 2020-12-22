1e18242841482e42606ae36f2d9f45cf175cb9c4
AttemptToCallNil <usergreenstone@gmail.com>
2020-12-22 21:30:32 +0500

    Extract SVG icons into separate files
    I had to remove some `inkscape:` attributes from the MAGE logo for
    it to work.

--- src/buttonmenu.vue
+++ src/buttonmenu.vue
@@ -1,4 +1,4 @@
-<template><svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg></template>
+<template><img width="20" height="20" :src="require('./assets/icons/menu.svg')"></template>
 <script>
 export default { };
 </script>
