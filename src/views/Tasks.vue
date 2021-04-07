b6c00b5207819cf5ab6e951bcfde7790360801fb
AttemptToCallNil <usergreenstone@gmail.com>
2021-04-08 02:09:18 +0500

    Implement a basic tasks UI stub (#57)
    No actual functionality yet.
    
    Known issue: the outer edges of "Enabled?" checkboxes have an area
    where the hover highlighting effect works, but clicking the area
    doesn't toggle the checkbox.

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,71 +1,90 @@
 <template>
-  <div id="loginentry" style="left: 0; top: 0; width: 100%; height: 100%; vertical-align: top; grid-template-columns: max-content auto; grid-template-rows: max-content max-content max-content max-content; grid-gap: 0.25em;" class="grid">
-
-    <div class="menu" title="The username for the wiki account.">
-      Account username
+  <div id="taskwindow" class="flex h-full">
+    <div id="pagelist" class="flex flex-col w-1/3 mx-1 flex-grow-0 bg-gray-100">
+      <h2 class="mx-auto my-1 text-2xl">Pages</h2>
+      <textarea class="border border-gray-400 h-full mx-1 resize-none text-sm font-mono my-auto"></textarea>
+      <div class="flex">
+        <button class="mt-1 mx-auto flex-shrink-0 hover:bg-gray-300 flex" title="Generate page list">
+          <svg-icon width="32" height="32" icon="plus" />
+        </button>
+      </div>
     </div>
-    <div class="menu">
-      <input type="text" id="account" name="name" class="w-full">
+    <div id="tasklist" class="flex flex-col w-2/3 mx-1 bg-gray-100">
+      <h2 class="mx-auto my-1 text-2xl">Current Tasks</h2>
+      <div class="overflow-y-auto my-1">
+        <div v-for="task in tasks" :key="task.internalId">
+          <div v-if="expandedTasks[task.internalId]" class="flex flex-col bg-gray-200 mx-1 my-0.5 px-1 py-1">
+            <div class="flex justify-between">
+              <button class="flex-shrink-0 opacity-60"><svg-icon width="24" height="24" icon="drag-indicator" /></button>
+              <h3 class="text-lg mx-auto">Task {{task.internalId}}: {{task.name}}</h3>
+              <button class="flex-shrink-0 hover:bg-gray-300" title="Collapse" @click="collapseTask(task.internalId)"><svg-icon width="24" height="24" icon="chevron-double-up" /></button>
+            </div>
+            <p class="mt-1 mb-3">{{task.description}}</p>
+            <div class="flex justify-between">
+              <button class="flex-shrink-0 hover:bg-gray-300" title="Task options"><svg-icon width="24" height="24" icon="cog" /></button>
+              <div class="select-none hover:bg-gray-300 px-1">
+                <label :for="'task-' + task.internalId + '-enabled'">Enabled? </label>
+                <input :id="'task-' + task.internalId + '-enabled'" type="checkbox" v-model="task.enabled">
+              </div>
+            </div>
+          </div>
+          <div v-else class="flex justify-between bg-gray-200 mx-1 my-0.5 px-1 py-1">
+            <button class="flex-shrink-0 opacity-60"><svg-icon width="24" height="24" icon="drag-indicator" /></button>
+            <h3 class="text-lg">Task {{task.internalId}}: {{task.name}}</h3>
+            <button class="flex-shrink-0 hover:bg-gray-300" title="Expand" @click="expandTask(task.internalId)"><svg-icon width="24" height="24" icon="chevron-double-down" /></button>
+          </div>
+        </div>
+      </div>
+      <div class="flex mt-auto">
+        <button class="mx-0.5 flex-shrink-0 hover:bg-gray-300" title="Options"><svg-icon width="32" height="32" icon="cog" /></button>
+        <button class="mr-auto flex-shrink-0 hover:bg-gray-300" title="Add task"><svg-icon width="32" height="32" icon="document-add" /></button>
+        <button class="mx-0.5 flex-shrink-0 hover:bg-gray-300" title="Run or resume tasks"><svg-icon width="32" height="32" icon="play" /></button>
+        <button class="mx-0.5 flex-shrink-0 hover:bg-gray-300" title="Pause all tasks to resume later"><svg-icon width="32" height="32" icon="pause" /></button>
+        <button class="mx-0.5 flex-shrink-0 hover:bg-gray-300" title="Abort all tasks (loses all progress)"><svg-icon width="32" height="32" icon="stop" /></button>
+      </div>
     </div>
-
-    <div class="menu" title="The BotPasswords username for generated credentials.">
-      Bot Password username
-    </div>
-    <div class="menu">
-      <input type="text" id="username" name="bpname" class="w-full">
-    </div>
-
-    <div class="menu" title="The BotPassword password for generated credentials. Do not use your original account password.">
-      Bot Password password
-    </div>
-    <div class="menu">
-      <input type="password" id="password" name="bppassword" class="w-full">
-    </div>
-
-    <div title="Add to existing wiki/farm">
-      <input type="checkbox" id="addToExisting" name="addToExisting" :value=false @change="addToExisting = !addToExisting">
-      <label for="addToExisting"> Add to existing wiki/farm </label>
-    </div>
-    <div v-show="addToExisting">
-      <label title="Farms this wiki may be a part of."> Farms: </label>
-      <Dropdown class="my-dropdown-toggle" :options="arrayOfObjects" :selected="object" v-on:updateOption="methodToRunOnSelect" :placeholder="'Select an Item'" :closeOnOutsideClick="true" />
-    </div>
-
-    <div title="Add to existing wiki/farm">
-      <input type="checkbox" id="addToExisting" name="addToExisting" :value=false @change="addToExisting = !addToExisting">
-      <label for="addToExisting"> Add to existing wiki/farm </label>
-    </div>
-    <div v-show="addToExisting">
-      <label title="Farms this wiki may be a part of."> Farms: </label>
-      <Dropdown class="my-dropdown-toggle" :options="arrayOfObjects" :selected="object" v-on:updateOption="methodToRunOnSelect" :placeholder="'Select an Item'" :closeOnOutsideClick="true" />
-    </div>
-
   </div>
 </template>
 
 <script>
-import Dropdown from "@/components/Dropdown.vue";
+import SvgIcon from "@/components/SvgIcon";
 
 export default {
-  components: { Dropdown },
+  components: { SvgIcon },
   data() {
     return {
-      addToExisting: false,
-
-      arrayOfObjects: [
-        { id: 1, name: "fandom.com" },
-        { id: 1, name: "gamepedia.com" }
+      expandedTasks: {},
+      tasks: [
+        {
+          internalId: 1,
+          name: "Null edit",
+          description: "Make a blank edit to the page to purge server-side cache.",
+          enabled: false
+        },
+        {
+          internalId: 2,
+          name: "Category change for patch notes",
+          description: "Renaming category 'PatchNotes' to 'Patch notes' to make the name less artificial.",
+          enabled: false
+        },
+        {
+          internalId: 3,
+          name: "Remove deprecated parameter",
+          description: "Remove the copy-paste legacy 'isCat' parameter from transclusions of Template:InfoboxCat. All cats are trivially cats.",
+          enabled: false
+        },
       ],
-      object: {
-        name: "None",
-      },
+      taskPages: [
 
-      farms: []
+      ]
     };
   },
   methods: {
-    methodToRunOnSelect(payload) {
-      this.object = payload;
+    expandTask(internalId) {
+      this.expandedTasks[internalId] = true;
+    },
+    collapseTask(internalId) {
+      this.expandedTasks[internalId] = false;
     }
   }
 };
