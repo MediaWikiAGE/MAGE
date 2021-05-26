4ab7b5a7ec3a5c1845d2ab4fc3a22b4547a1c32c
AttemptToCallNil <usergreenstone@gmail.com>
2021-05-26 18:26:19 +0500

    Home screen: wiki/account selection UI (#68)
    * Add visual elements to the home screen
    
    * Hook up account data for the home screen account selection
    
    The interface works, but the choice isn't retained. Will have to
    work on writing to the central store later. Hopefully not much
    later.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,59 @@
 <template>
-  <div>
-    &nbsp;
+  <div class="dark:bg-gray-900 h-full">
+    <div class="w-full text-center">
+      <h2 class="text-3xl">Welcome to MAGE</h2>
+    </div>
+
+    <div class="mt-3 ml-2">
+      <h3 class="text-xl">Choose a wiki to operate on:</h3>
+
+      <div class="max-w-xs">
+        <div class="select-none flex flex-col w-full">
+          <label for="chosenFarm" class="pr-1">Wiki farm name</label>
+          <select id="chosenFarm" name="chosenFarm" class="flex-grow dark:bg-gray-700" :value="chosenFarm" @change="onFarmChange">
+            <option value="-1" selected>None (standalone wiki)</option>
+            <option v-for="(knownFarm, index) in chosenFarmSource" :key="knownFarm.name" :value="index">{{ knownFarm.name }}</option>
+          </select>
+        </div>
+
+        <div class="select-none flex flex-col w-full mt-2">
+          <label for="chosenWiki" class="pr-1">Wiki name</label>
+          <select id="chosenWiki" name="chosenWiki" class="flex-grow dark:bg-gray-700" :value="chosenWiki" @change="onWikiChange">
+            <option value="-1" disabled selected>None</option>
+            <option v-for="(knownWiki, index) in chosenWikiSource" :key="knownWiki.name" :value="index">{{ knownWiki.name }}</option>
+          </select>
+        </div>
+
+        <div class="select-none flex flex-col w-full mt-2">
+          <label for="chosenAccount" class="pr-1">User account</label>
+          <select id="chosenAccount" name="chosenAccount" class="flex-grow dark:bg-gray-700" :value="chosenAccount" @change="onAccountChange">
+            <option value="-1" selected>None (identify using your IP)</option>
+            <option v-for="(knownAccount, index) in chosenAccountSource" :key="knownAccount.name" :value="index">{{ knownAccount.name }}</option>
+          </select>
+        </div>
+      </div>
+
+      <p class="mt-3 text-sm italic">MAGE will log in automatically as needed to run tasks</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  data: () => ({ users: [], name: null, wiki: null }),
+  data() {
+    return {
+      chosenFarm: -1,
+      chosenWiki: -1,
+      chosenAccount: -1,
+      accountData: {},
+      chosenFarmSource: [],
+      chosenWikiSource: [],
+      chosenAccountSource: []
+    };
+  },
   methods: {
+    /*
     login(event) {
       const userKey = event.target.dataset.id;
       window.api.remote("loginUser", userKey).then(data => {
@@ -26,11 +71,39 @@ export default {
         this.$store.state.current_user = { ...data.cacheUser, ...data.cacheSite };
       });
     }
+    */
+    onFarmChange(event) {
+      this.chosenFarm = parseInt(event.target.value, 10);
+      this.chosenWiki = -1;
+      this.chosenAccount = -1;
+
+      if (this.chosenFarm === -1) {
+        this.chosenWikiSource = this.accountData.standalone;
+        this.chosenAccountSource = [];
+      } else {
+        const farmData = this.accountData.farms[this.chosenFarm];
+        this.chosenWikiSource = farmData.wikis;
+        this.chosenAccountSource = farmData.accounts;
+      }
+    },
+    onWikiChange(event) {
+      this.chosenWiki = parseInt(event.target.value, 10);
+
+      if (this.chosenFarm === -1) {
+        const wikiData = this.accountData.standalone[this.chosenWiki];
+        this.chosenAccount = -1;
+        this.chosenAccountSource = wikiData.accounts;
+      }
+    },
+    onAccountChange(event) {
+      this.chosenAccount = parseInt(event.target.value, 10);
+    }
   },
-  created() {
-    window.api.remote("getUserLists").then(data => {
-      this.users = data;
-    });
-  }
+  created: async function() {
+    const accountData = await window.api.remote("getAccountData");
+    this.accountData = accountData;
+    this.chosenFarmSource = accountData.farms;
+    this.chosenWikiSource = accountData.standalone;
+  },
 };
 </script>
