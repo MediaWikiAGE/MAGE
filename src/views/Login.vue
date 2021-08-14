926a449f6a944d763639fa9055158c734f902d6c
AttemptToCallNil <usergreenstone@gmail.com>
2021-08-15 00:23:47 +0500

    Refactor the login form to remove both checkboxes
    Advantages:
     - No longer possible to have a problematic case when the user selects
       "add to existing", but there are no wiki farms or standalone wikis
       to add new credentials to.
     - Simplifies some internal code a bit. Also changed some variable names
       to be clearer.
    Disadvantages:
     - May not make it clear that it's needed to enter that dropdown if one wants
       to set up a new standalone wiki, rather than a new wiki farm. I'm looking
       to address it in the next commit(s). Thanks to River for pointing that
       out.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="loginentry" class="2xl:grid 2xl:grid-cols-loginview-outer h-full dark:bg-gray-900">
     <div class="grid grid-cols-loginview-3 lg:grid-cols-loginview-4 gap-x-1 gap-y-4 2xl:col-start-2">
-      <div class="hidden select-none lg:flex lg:justify-center lg:items-center lg:col-start-4 lg:row-start-1 lg:row-end-9"><svg-icon icon="magelogo" noinvert="true" /></div>
-      <div class="flex justify-center row-start-1 row-end-9 col-start-2">
+      <div class="hidden select-none lg:flex lg:justify-center lg:items-center lg:col-start-4 lg:row-start-1 lg:row-end-8"><svg-icon icon="magelogo" noinvert="true" /></div>
+      <div class="flex justify-center row-start-1 row-end-8 col-start-2">
         <div class="w-px border-l border-gray-500"></div>
       </div>
 
@@ -31,55 +31,44 @@
       </div>
 
       <div class="help-text row-start-4">
-        Do you want to add the credentials for a wiki or wiki farm already known to MAGE?
+        Do you want to add the credentials for a wiki or wiki farm already known to MAGE, or do you want to set up a new wiki or farm?
       </div>
       <div class="form-input-wrapper row-start-4">
-        <div>
-          <label for="addToExisting" class="pr-1">Add to existing?</label>
-          <input type="checkbox" id="addToExisting" name="addToExisting" :value=false v-model="addToExisting">
-        </div>
-        <select :disabled="!addToExisting" name="addTo" id="addTo" v-model.number="addTo">
-          <option value="0" disabled selected>None</option>
+        <select name="addTo" id="addTo" v-model.number="addTo">
+          <option value="-2" selected>None (set up a new wiki farm)</option>
+          <option value="-1" selected>None (set up a new standalone wiki)</option>
           <option v-for="knownFarm in farms" :key="knownFarm.id" :value="knownFarm.id">{{ knownFarm.name }}</option>
         </select>
       </div>
 
       <div class="help-text row-start-5">
-        Do you use the same credentials to log into several different wikis?
-      </div>
-      <div class="form-input-wrapper row-start-5">
-        <label for="isWikiFarm" class="pr-1">Wiki farm?</label>
-        <input type="checkbox" id="isWikiFarm" name="isWikiFarm" v-model="isWikiFarm" :disabled="addToExisting">
-      </div>
-
-      <div class="help-text row-start-6">
-        <div v-if="!isWikiFarm">The front page of your wiki. MAGE will figure out the script path automatically.</div>
+        <div v-if="creatingStandaloneWiki">The front page of your wiki. MAGE will figure out the script path automatically.</div>
         <div v-else>The front pages of your wikis, one per line. MAGE will figure out the script path automatically.</div>
       </div>
-      <div class="form-input-wrapper row-start-6">
-        <div v-if="!isWikiFarm">
+      <div class="form-input-wrapper row-start-5">
+        <div v-if="creatingStandaloneWiki">
           <label for="urlField">URL</label>
-          <input type="text" id="urlField" name="urlField" :value="wikiUrls[0]" @input="onUrlFieldInput" :disabled="addToExisting">
+          <input type="text" id="urlField" name="urlField" :value="wikiUrls[0]" @input="onUrlFieldInput" :disabled="addingToExistingWikiOrFarm">
         </div>
         <div v-else>
           <label for="urlArea">URLs</label>
-          <textarea id="urlArea" name="urlArea" :value="wikiUrls.join('\n')" @input="onUrlAreaInput" rows="4" :disabled="addToExisting"></textarea>
+          <textarea id="urlArea" name="urlArea" :value="wikiUrls.join('\n')" @input="onUrlAreaInput" rows="4" :disabled="addingToExistingWikiOrFarm"></textarea>
         </div>
       </div>
 
-      <div class="help-text row-start-7">
-        <div v-if="!isWikiFarm">Save this wiki as:</div>
+      <div class="help-text row-start-6">
+        <div v-if="creatingStandaloneWiki">Save this wiki as:</div>
         <div v-else>Save this farm as:</div>
       </div>
-      <div class="form-input-wrapper row-start-7">
-        <input type="text" id="saveAs" name="saveAs" v-model.trim="saveAs" :disabled="addToExisting">
+      <div class="form-input-wrapper row-start-6">
+        <input type="text" id="saveAs" name="saveAs" v-model.trim="saveAs" :disabled="addingToExistingWikiOrFarm">
       </div>
 
-      <div class="form-input-wrapper row-start-8">
+      <div class="form-input-wrapper row-start-7">
         <input type="button" id="saveLogin" name="saveLogin" value="Save login" @click="saveLogin" :disabled="blockSaving" class="p-0.5">
       </div>
 
-      <div class="row-start-9 col-start-1 col-end-5">
+      <div class="row-start-8 col-start-1 col-end-5">
         <span v-if="validationErrors.length > 0">Errors:</span>
         <ul class="bg-gray-100 dark:bg-gray-800 text-red-900 dark:text-red-400 text-sm">
           <li v-for="(error, index) in validationErrors" :key="index">{{ error }}</li>
@@ -98,9 +87,7 @@ export default {
       accountName: null,
       botPasswordName: null,
       botPassword: null,
-      addToExisting: false,
-      addTo: 0,
-      isWikiFarm: false,
+      addTo: -2,
       wikiUrls: [],
       saveAs: null,
       blockSaving: false,
@@ -113,15 +100,24 @@ export default {
       },
     };
   },
+  computed: {
+    creatingWikiFarm() {
+      return this.addTo === -2;
+    },
+    creatingStandaloneWiki() {
+      return this.addTo === -1;
+    },
+    addingToExistingWikiOrFarm() {
+      return this.addTo >= 0;
+    }
+  },
   methods: {
     commitChoices() {
       const loginData = {
         accountName: this.accountName,
         botPasswordName: this.botPasswordName,
         botPassword: this.botPassword,
-        addToExisting: this.addToExisting,
         addTo: this.addTo,
-        isWikiFarm: this.isWikiFarm,
         wikiUrls: this.wikiUrls,
         saveAs: this.saveAs,
         validationErrors: this.validationErrors
@@ -144,9 +140,6 @@ export default {
     },
     onUrlAreaInput(event) {
       this.wikiUrls = event.target.value.split("\n");
-    },
-    switchWikiFarmFlag(event) {
-      this.isWikiFarm = !this.isWikiFarm;
     },
     /// Returns `false` if `string` is a string with at least one significant character, otherwise `true`.
     isEmpty(string) {
@@ -177,12 +170,8 @@ export default {
       if (this.isEmpty(this.botPassword)) {
         validationErrors.push("The bot password cannot be empty");
       }
-      if (this.addToExisting) {
-        if (this.addTo === 0) {
-          validationErrors.push("When adding to an existing wiki or farm, the wiki or farm needs to be selected");
-        }
-      } else {
-        if (!this.isWikiFarm) {
+      if (!this.addingToExistingWikiOrFarm) {
+        if (this.creatingStandaloneWiki) {
           if (this.isEmpty(this.wikiUrls[0])) {
             validationErrors.push("When creating a new standalone wiki, the wiki URL cannot be empty");
           } else if (!this.isValidUrl(this.wikiUrls[0])) {
@@ -192,7 +181,7 @@ export default {
           if (this.farms.some(authSystem => authSystem.name === this.saveAs && !authSystem.isFarm)) {
             validationErrors.push(`A standalone wiki named "${this.saveAs}" is already defined.`);
           }
-        } else {
+        } else if (this.creatingWikiFarm) {
           let noValidUrls = true;
           for (const url of this.wikiUrls) {
             if (this.isEmpty(url)) {
@@ -214,8 +203,8 @@ export default {
           }
         }
 
-        if (this.saveAs === null || this.saveAs === "") {
-          validationErrors.push("When adding to an existing wiki or farm, the name for the wiki or farm cannot be empty");
+        if (this.isEmpty(this.saveAs)) {
+          validationErrors.push("When creating a new wiki or farm, the name for the wiki or farm cannot be empty");
         }
       }
 
@@ -227,9 +216,7 @@ export default {
       this.accountName = null;
       this.botPasswordName = null;
       this.botPassword = null;
-      this.addToExisting = false;
-      this.addTo = 0;
-      this.isWikiFarm = false;
+      this.addTo = -2;
       this.wikiUrls = [];
       this.saveAs = null;
       this.farms = await window.api.remote("getAuthSystemList");
@@ -240,9 +227,9 @@ export default {
 
     /// Separate function for (asynchronously) updating settings when saving a login.
     async updateSettings(authSystemData, botPasswordData) {
-        if (this.addToExisting) {
+        if (this.addingToExistingWikiOrFarm) {
           await window.api.remote("addBotPasswordForAuthSystem", authSystemData, botPasswordData);
-        } else if (this.isWikiFarm) {
+        } else if (this.creatingWikiFarm) {
           // this.wikiUrls gives an "An object could not be cloned." error if provided directly?
           await window.api.remote("createWikiFarmWithUrls", this.saveAs, [...this.wikiUrls]);
           await window.api.remote("addBotPasswordForAuthSystem", authSystemData, botPasswordData);
@@ -265,7 +252,7 @@ export default {
         };
 
         // The auth systems list has structures with the same fields as needed by auth system remotes.
-        const authSystemData = this.addToExisting
+        const authSystemData = this.addingToExistingWikiOrFarm
           ? { ... this.farms[this.addTo - 1] }
           : {
             name: this.saveAs,
